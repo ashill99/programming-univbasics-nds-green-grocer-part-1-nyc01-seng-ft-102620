efc422760ea383978e9829bd2496a135c36725dd
Adam Shilling <admshng@gmail.com>
2020-10-09 18:59:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -22,7 +22,7 @@ counter = 0
 while counter < cart.length 
   new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
   if new_cart_item != nil 
-    new_cart_item[:count] + 1
+    new_cart_item[:count] += 1
   counter += 1
 
   end 
